d388cafd33d742a2a97012cfeee1cf7ac0724715
Eric Onuoha <plipsd@gmail.com>
2022-11-24 17:12:38 +0100

    Add Mobile view on home page - Complete newsPreviews and latestPrograms

--- src/components/newsPreviews/newsPreviews.component.jsx
+++ src/components/newsPreviews/newsPreviews.component.jsx
@@ -14,18 +14,18 @@ const NewsPreviews = () =>{
 
             <div id="newsContent">
                 <div id="newsTitle">
-                    <h1>Sample News Article: WIPF LAUNCH ORGANIZATION WEBSITE</h1>
+                    <h2 className="h2">Sample News Article: WIPF LAUNCH ORGANIZATION WEBSITE</h2>
                 </div>
                 <div id="newsPreview">
                     <div id="newsPreviewCover">
-                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odio quisquam adipisci et expedita 
+                    <p className="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odio quisquam adipisci et expedita 
                         perspiciatis nam dolorem doloribus asperiores, corporis ad officia rem aspernatur architecto? 
                         Id quo vel consequatur enim debitis, ipsam nemo quibusdam quae pariatur laudantium. Sint minima 
-                        culpa nesciunt beatae! Atque, quo suscipit neque est expedita animi cum.</h4> 
+                        culpa nesciunt beatae! Atque, quo suscipit neque est expedita animi cum.</p> 
                         <div id="newsMetadata">
                             <img src={Divider} alt="" />
-                            <h5>THISDAY NEWSPAPERS</h5>
-                            <p>12th July 2022</p>
+                            <h4 className="h4">THISDAY NEWSPAPERS</h4>
+                            <h5 className="h5">12th July 2022</h5>
                         </div>
                     </div>                       
                     <div id="newsToggle">
