b23dc4a0e53035d4d5849fd5bb47122e7a7ad4ca
Eric Onuoha <plipsd@gmail.com>
2022-11-27 21:22:59 +0100

    add twitter to footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,5 +1,6 @@
 import "../footer/footer.styles.scss";
 import {Timeline} from 'react-twitter-widgets';
+import { InstagramEmbed } from 'react-social-media-embed';
 
 // import WIPFLogo from "../wipfLogo/wipfLogo.component";
 
@@ -20,11 +21,11 @@ const Footer = () =>{
                     </div>
                 </div>
                 <div id="footerInstagram">
-                    
+                    <InstagramEmbed url="https://www.instagram.com/p/CUbHfhpswxt/" width={328} captioned />
                 </div>
                 <div id="footerTwitter">
-                <a className="twitter-timeline" data-width="95%" data-height="450px" href="https://twitter.com/wipfng?ref_src=twsrc%5Etfw">Tweets by wipfng</a> 
-                <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script> 
+                    <a className="twitter-timeline" href="https://twitter.com/wipfng?ref_src=twsrc%5Etfw">Tweets by wipfng</a> 
+                    <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script> 
                     <Timeline/>                
                 </div>
             </div>            
