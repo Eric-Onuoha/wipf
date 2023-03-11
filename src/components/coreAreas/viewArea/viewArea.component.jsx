488e8a431539ca85018801e08f4d78c00f0eaeff
Eric Onuoha <plipsd@gmail.com>
2023-03-11 21:28:04 +0100

    View Core area redesign

--- src/components/coreAreas/viewArea/viewArea.component.jsx
+++ src/components/coreAreas/viewArea/viewArea.component.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import "./viewArea.styles.scss";
 import parse from "html-react-parser";
+import bannerImage from "../../../assets/images/press conference.jpg";
+import mentorship from "../../../assets/images/mentorship.jpg";
+import advocacy from "../../../assets/images/advocacy.jpg";
 
 const ViewArea = () => {
     
@@ -10,24 +13,30 @@ const CoreAreasDB = useSelector((state) => state.coreAreas.coreAreas.CoreAreas) 
 
 const areaTitles = Object.keys(CoreAreasDB);
 const areaContent = Object.values(CoreAreasDB);
+console.log(areaTitles);
+
+const showImage = (area) =>{
+    if(area === "Political Advocacy"){
+        return <div>
+                <img src={advocacy} alt="" /> 
+                <p>Women in Politics forum present in Parliament advocatong for bills in support of women</p>
+            </div>;
+    } else if (area === "Mentorship & Training"){
+        return <img src={mentorship} alt="" />;
+    } else {
+        return <img src={bannerImage} alt="" />
+    }
+}
 
     return(
         <div id="coreAreaComponent">
         {areaTitles.map((areaTitle, i) => (
-          <div className="coreArea" key={areaTitle}>
-            <div id="coreAreaImage">
-                <img src="" alt="" />
-            </div>
-            <div>
-                <div id="coreAreaHeading">
-                    <h1 className="h2">{areaTitle}</h1>
-                </div>
-                <div>
-                    <p id="coreAreasText" className="p2">
-                        {parse(areaContent[i])}
-                    </p>
-                </div>
-            </div>
+          <div className="coreAreaContent" key={areaTitle}>
+            {showImage(areaTitle)}
+            <h1 className="h2">{areaTitle}</h1>
+            <p id="coreAreaText" className="p2">
+                {parse(areaContent[i])}
+            </p>
           </div>
         ))}
       </div>
