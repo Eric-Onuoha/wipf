6d0f8fc8d7c18e2b731b1ddd32642c6cc3758546
Eric Onuoha <plipsd@gmail.com>
2023-03-21 16:02:49 +0100

    Responsive design of wipfteam component for ipad view

--- src/reduxStore/reduxStore.js
+++ src/reduxStore/reduxStore.js
@@ -1,9 +1,9 @@
 import {compose, createStore, applyMiddleware} from "redux"; // see if creat store and configureStore are interchangable
 // import logger from "react-redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { rootReducer } from "./rootReducer";
 
 
-const middlewares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middlewares));
-export const reduxStore = createStore(rootReducer, undefined, composedEnhancers);
+// const middlewares = [logger];
+// const composedEnhancers = compose(applyMiddleware(...middlewares));
+export const reduxStore = createStore(rootReducer);
